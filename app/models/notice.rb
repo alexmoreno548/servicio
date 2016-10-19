f459afe3b535bf1542c327fdb01850800c4ebf3a
Alexis Moreno <alexmoreno548@gmail.com>
2016-10-19 13:32:23 -0400

    test on paperclip

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,4 @@
 class Notice < ApplicationRecord
-	has_attached_file :image, styles: {large: "800x600>", medium: "400x300>"}
+	has_attached_file :image, styles: {large: "800x600>", medium: "400x300>"}, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
